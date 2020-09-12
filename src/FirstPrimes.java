7106bc777d666bce0f0cbfe2a9ac495ebfdea474
kkingkangg <phungnghia152@gmail.com>
2020-09-12 16:46:28 +0700

    Số các số nguyên tố đầu tiên

---
+++ src/FirstPrimes.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class FirstPrimes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập số các số nguyên tố đầu tiên bạn muốn tìm");
+        int limit = input.nextInt();
+        int cout = 1;
+        int num = 2;
+
+        while (num<=limit){
+            if (checkPrime(num)) {
+                System.out.print(num + ",");
+                cout++;
+            }
+            num++;
+        }
+
+    }
+
+    static boolean checkPrime(int n) {
+            for (int i = 2; i <= Math.sqrt(n) ; i++) {
+                if (n%i==0)
+                    return false;
+            }
+            return true;
+    }
+
+}
